b36904cce57a387b874bc52d7e29363fca8b38e6
Elisa Bertoni <bertoni_elisa@hotmail.com>
2023-04-28 17:59:48 +1200

    install auth0

---
+++ server/routes/collections.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import checkJwt from '../'
+
+const router = express.Router()
+
+
+router.get
+
+export default router
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import { join } from 'node:path'
 
-import fruitRoutes from './routes/fruits'
-
 const server = express()
+
+import collections from './routes/collections'
 
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
+server.use('/api/v1/collections', collections)
 
-server.use('/api/v1/fruits', fruitRoutes)
 
 export default server
